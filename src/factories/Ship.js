0a782d0c2217d2b88874ce0c2573642910f4b770
Sullivown <daniel.w.t.sullivan@gmail.com>
2022-04-05 13:53:02 +0100

    Add ship methods - hit, length, isSunk

--- src/factories/Ship.js
+++ src/factories/Ship.js
@@ -1,5 +1,42 @@
-function Ship() {
-	return 'ship';
+function Ship(length) {
+	let hits = initHits();
+	const getLength = () => length;
+	const getHits = () => hits;
+
+	// Hit location initializer
+	function initHits() {
+		let hits = [];
+		for (let i = 0; i < length; i++) {
+			hits.push(false);
+		}
+		return hits;
+	}
+
+	const hit = (location) => {
+		hits[location] = true;
+	};
+
+	const isSunk = () => {
+		const totalHits = hits.reduce((total, curr) => {
+			if (curr == true) {
+				total += 1;
+			}
+			return total;
+		}, 0);
+
+		if (totalHits == length) {
+			return true;
+		} else {
+			return false;
+		}
+	};
+
+	return {
+		getLength,
+		getHits,
+		hit,
+		isSunk,
+	};
 }
 
 export default Ship;
--- src/factories/Ship.test.js
+++ src/factories/Ship.test.js
@@ -1,6 +1,20 @@
 import Ship from './Ship';
 
-test('Happy path create ship', () => {
-	let ship = Ship();
-	expect(ship).toBe('ship');
+test('Ship object creation', () => {
+	let ship = Ship(3);
+	expect(ship.getLength()).toEqual(3);
+});
+
+test('Ship hit method', () => {
+	let ship = Ship(4);
+	ship.hit(0);
+	expect(ship.getHits()).toEqual([true, false, false, false]);
+});
+
+test('Ship isSunk method', () => {
+	let ship = Ship(2);
+	ship.hit(0);
+	ship.hit(1);
+
+	expect(ship.isSunk()).toBe(true);
 });
